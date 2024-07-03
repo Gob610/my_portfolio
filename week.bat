41f04ddf40d8627419ad063c3479e3398bcd894a
Gonzalo Barrios <119068744+Gob610@users.noreply.github.com>
2024-07-02 21:55:22 -0500

    Delete week.bat

--- week.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd C:\Users\ATSISTEMASDEV\Documents\Proyectos\ETL_AgroBrain\etl-agro
-call Scripts\activate.bat
-cd C:\Users\ATSISTEMASDEV\Documents\Proyectos\ETL_AgroBrain
-python main_week.py
